9d7548abc9613f95d57b1da1af32e7ee512b2c6f
danielcorfer <danielmazura@gmail.com>
2022-09-24 14:06:10 +0200

    add CheckSameCaseAlg

---
+++ CheckSameCase/CheckSameCase/Program.cs
@@ -0,0 +1,25 @@
+﻿
+public class Program
+{
+    static void Main(string[] args)
+    {
+        char letter = 'a';
+        Console.WriteLine($"{letter.GetType().Name}");
+
+        char letter2 = 'b';
+        Console.WriteLine(SameCase(letter, letter2));
+    }
+
+
+   public static int SameCase(char firstLetter, char secondLetter)
+    {
+        if (!char.IsLetter(firstLetter) || !char.IsLetter(secondLetter))
+        {
+            return -1;
+        }
+//IsLower() returns bool => two bools are then compared and return integer
+                   
+        return char.IsLower(firstLetter) == char.IsLower(secondLetter) ? 1 : 0;
+    }
+}
+
